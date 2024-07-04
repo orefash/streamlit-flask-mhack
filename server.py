bd24a46ae61f77c7fbb9052df80f564238830080
orefash <orefash@gmail.com>
2024-07-04 20:36:07 +0100

    update flask

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, World!"
 
+@app.post('/query')
+def data_query():
+    print("In test query req")
+    query = request.json['query']
+
+    print("In test query req: Query - ", query)
+    # response = get_response(query)
+
+    # response_data = {
+    #     "query" : query,
+    #     "response" : response
+    # }
+
+    # return jsonify(response_data)
+    return jsonify({"start": "start"})
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5005)
